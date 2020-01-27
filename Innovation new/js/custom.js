ebba61623a95a1f45d5b63ad4425a8a17778d055
LakshaySinghal-Dev <60272166+LakshaySinghal-Dev@users.noreply.github.com>
2020-01-28 03:17:05 +0530

    Add files via upload

---
+++ Innovation new/js/custom.js
@@ -0,0 +1,122 @@
+/* HTML document is loaded. DOM is ready.
+-------------------------------------------*/
+$(function(){
+
+    /* start typed element */
+    //http://stackoverflow.com/questions/24874797/select-div-title-text-and-make-array-with-jquery
+    var subElementArray = $.map($('.sub-element'), function(el) { return $(el).text(); });
+    $(".element").typed({
+        strings: subElementArray,
+        typeSpeed: 30,
+        contentType: 'html',
+        showCursor: false,
+        loop: true,
+        loopCount: true,
+    });
+    /* end typed element */
+
+    /* Smooth scroll and Scroll spy (https://github.com/ChrisWojcik/single-page-nav)
+    ---------------------------------------------------------------------------------*/
+    $('.templatemo-nav').singlePageNav({
+        offset: $(".templatemo-nav").height(),
+        filter: ':not(.external)',
+        updateHash: false
+    });
+
+    /* start navigation top js */
+    $(window).scroll(function(){
+        if($(this).scrollTop()>58){
+            $(".templatemo-nav").addClass("sticky");
+        }
+        else{
+            $(".templatemo-nav").removeClass("sticky");
+        }
+    });
+
+    /* Hide mobile menu after clicking on a link
+    -----------------------------------------------*/
+    $('.navbar-collapse a').click(function(){
+        $(".navbar-collapse").collapse('hide');
+    });
+    /* end navigation top js */
+
+    $('body').bind('touchstart', function() {});
+
+    /* wow
+    -----------------*/
+    new WOW().init();
+});
+
+/* start preloader */
+$(window).load(function(){
+	$('.preloader').fadeOut(1000); // set duration in brackets
+});
+/* end preloader */
+
+
+function submitResponse()
+{
+    document.forms[0].action ="http://www.nitj.ac.in/";
+    document.forms[0].submit();
+}
+
+function redirect()
+{
+    var name =document.getElementById("fullname");
+    var email=document.getElementById("email");
+    var message=document.getElementById("message");
+
+}
+
+// function to show more data in patrons
+function myFunction() {
+  var dots = document.getElementById("director_read_more");
+  var moreText = document.getElementById("more_director_info");
+  var btnText = document.getElementById("clicked_on_director_info");
+
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+
+}
+function myFunction1() {
+  var dots = document.getElementById("dean_read_more");
+  var moreText = document.getElementById("more_dean_info");
+  var btnText = document.getElementById("clicked_on_dean_info");
+
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+
+}
+function myFunction2() {
+  var dots = document.getElementById("coordinator_read_more");
+  var moreText = document.getElementById("more_coordinator_info");
+  var btnText = document.getElementById("clicked_on_coordinator_info");
+
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+
+}
